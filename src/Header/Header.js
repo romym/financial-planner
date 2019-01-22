40e9bc06748d914b09da3b0bc6a592dad4da30d9
Dylan A <dylanpatrickavery@gmail.com>
2019-01-22 08:47:40 -0700

    Finish basic css for frontend

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -19,6 +19,7 @@ class Header extends Component {
         <p className="header-links" onClick={() => this.createNewUser()}>New User</p>
         <p className={`${this.state.transactionDisabled}`} onClick={() => this.createNewUser()}>New Budget</p>
         <p className={`${this.state.transactionDisabled}`} onClick={() => this.createNewUser()}>Add Transaction</p>
+        <p className="todays-date">1/01/10</p>
       </div>
     );
   }
